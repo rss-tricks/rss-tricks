838be56467738300d25ad762fde605a716330682
fimion <fimion@gmail.com>
2021-12-10 23:23:24 -0500

    Dates and stuff

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,13 @@
 module.exports = function (eleventy) {
   eleventy.addPassthroughCopy({"src/assets": "assets"})
   eleventy.setTemplateFormats(["html", "liquid", "ejs", "md", "hbs", "mustache", "haml", "pug", "njk", "11ty.js", "xml"])
+
+  eleventy.addFilter("ISOString", (v)=>v.toISOString())
+
   eleventy.dir = {
     input: "src",
   }
+
   return eleventy;
+
 }
--- src/src.11tydata.js
+++ src/src.11tydata.js
@@ -1,2 +1,4 @@
 module.exports = {
+  URL: process.env.URL,
+  currentDate: new Date().toISOString(),
 }
